b0f683ba8f148cdaf47cd54e203693f74c5ccbdc
Rudi Abrahamsen <rudi.abrah@gmail.com>
2023-04-04 12:31:29 +0200

    Started shaping the gui

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextStream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
